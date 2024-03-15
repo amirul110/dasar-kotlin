79cb1d30980d0f5266f332241d54505d84bbcb75
amirurich02 <muhammadirul04@gmail.com>
2024-03-15 10:07:59 +0700

    hallo

---
+++ Function.kt
@@ -0,0 +1,22 @@
+package Function
+
+import operasi.tiga
+
+/*
+sebelumnya udah buat func yaitu func main
+func main adalah sebuah blok kode yang sengaja dibat dalam program agar bisa digunakan berulang-ulang
+cara membuat function di kotlin sanagt sederhana,hanya menggunakan kata kunci fun lalu dii kuti nama function nya dan blok kode isi function
+setelah membuat function,kita bisa mengeksekusi function tersebut dengan memanggilnya
+menggunakan kata kunci nama function nya
+ */
+
+fun halloDunia(){
+    println("selamat sore bang!")
+    println("amirul muchminiin")
+}
+
+fun main(){
+    halloDunia()
+    halloDunia()
+    tiga()
+}
